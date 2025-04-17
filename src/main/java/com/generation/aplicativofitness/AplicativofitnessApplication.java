183982dac3bd7ce8c1063ab7e07c40874f6feec3
GiulioArantes <giulio.arantes@icloud.com>
2025-04-17 09:54:06 -0300

    feat: criação do projeto Spring Boot com todas as dependências necessárias

---
+++ src/main/java/com/generation/aplicativofitness/AplicativofitnessApplication.java
@@ -0,0 +1,13 @@
+package com.generation.aplicativofitness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativofitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicativofitnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/aplicativofitness/AplicativofitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.aplicativofitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicativofitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

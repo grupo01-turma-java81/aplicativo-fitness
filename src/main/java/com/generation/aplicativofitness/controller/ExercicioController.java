b640ee0eadca1f09a459e368a64bd0b15ff30b5f
ViniCristhian <viniciuscristhian34@gmail.com>
2025-04-17 12:29:00 -0300

    feat: finalização do CRUD básico das classes Treino e Exercicio Controller

--- src/main/java/com/generation/aplicativofitness/controller/ExercicioController.java
+++ src/main/java/com/generation/aplicativofitness/controller/ExercicioController.java
@@ -1,26 +1,64 @@
 package com.generation.aplicativofitness.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.aplicativofitness.model.Exercicio;
 import com.generation.aplicativofitness.repository.ExercicioRepository;
 
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RestController
 @RequestMapping("/exercicio")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class ExercicioController {
 
     @Autowired
     private ExercicioRepository exercicioRepository;
-    
+
     @GetMapping
     public ResponseEntity<List<Exercicio>> getAll() {
 	return ResponseEntity.ok(exercicioRepository.findAll());
     }
-    
+
+    @PostMapping
+    public ResponseEntity<Exercicio> post(@Valid @RequestBody Exercicio exercicio) {
+	return ResponseEntity.status(HttpStatus.CREATED).body(exercicioRepository.save(exercicio));
+    }
+
+    @PutMapping
+    public ResponseEntity<Exercicio> put(@Valid @RequestBody Exercicio exercicio) {
+	return exercicioRepository.findById(exercicio.getId())
+		.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+			.body(exercicioRepository.save(exercicio)))
+		.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+	Optional<Exercicio> exercicio = exercicioRepository.findById(id);
+
+	if (exercicio.isEmpty()) {
+	    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	}
+
+	exercicioRepository.deleteById(id);
+    }
+
 }
--- src/main/java/com/generation/aplicativofitness/controller/TreinoController.java
+++ src/main/java/com/generation/aplicativofitness/controller/TreinoController.java
@@ -1,16 +1,27 @@
 package com.generation.aplicativofitness.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.aplicativofitness.model.Treino;
 import com.generation.aplicativofitness.repository.TreinoRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/treino")
@@ -19,9 +30,34 @@ public class TreinoController {
 
     @Autowired
     private TreinoRepository treinoRepository;
-    
+
     @GetMapping
     public ResponseEntity<List<Treino>> getAll() {
 	return ResponseEntity.ok(treinoRepository.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Treino> post(@Valid @RequestBody Treino treino) {
+	return ResponseEntity.status(HttpStatus.CREATED).body(treinoRepository.save(treino));
+    }
+
+    @PutMapping
+    public ResponseEntity<Treino> put(@Valid @RequestBody Treino treino) {
+	return treinoRepository.findById(treino.getId())
+		.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+			.body(treinoRepository.save(treino)))
+		.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+    
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+	Optional<Treino> treino = treinoRepository.findById(id);
+	
+	if (treino.isEmpty()) {
+	    throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	}
+	
+	treinoRepository.deleteById(id);
     }
 }
